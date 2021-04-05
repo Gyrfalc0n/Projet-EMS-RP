ebe36fce4600b2d41afa05b9847d7d9baaaa6d95
Enguerrand Vié <enguerrandvie@gmail.com>
2021-04-05 23:41:59 +0200

    udpate

---
+++ layout.err.tcl
@@ -0,0 +1,24 @@
+unknown option "vTcl:DefineAlias"
+    while executing
+"label $site_4_0.lab45  -activebackground #f9f9f9 -activeforeground black  -background $vTcl(actual_gui_bg) -disabledforeground #a3a3a3  -font TkDefaul..."
+    ("uplevel" body line 62)
+    invoked from within
+"uplevel 1 $body"
+    invoked from within
+"vTcl:withBusyCursor {
+    menu $top.m46  -activebackground $vTcl(analog_color_m) -activeforeground #000000  -background $vTcl(pr,menubgcolor) -font Tk..."
+    (procedure "vTclWindow.top44" line 29)
+    invoked from within
+"vTclWindow.top44 .top44 "
+    ("eval" body line 1)
+    invoked from within
+"eval "vTclWindow$name $newname $rest""
+    (procedure "Window" line 18)
+    invoked from within
+"Window show .top44 $btop"
+    (file "E:/Projet EMS RP/Projet-EMS-RP/layout.tcl" line 695)
+    invoked from within
+"source {E:/Projet EMS RP/Projet-EMS-RP/layout.tcl}"
+    ("uplevel" body line 1)
+    invoked from within
+"uplevel #0 [list source $file]"
--- main.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-
-root = tk.Tk()
-
-
-
-# execute window
-root.mainloop()
