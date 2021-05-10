6cd1464ebddb69684e11fb829056edda7d585920
Enguerrand Vié <enguerrandvie@gmail.com>
2021-05-10 15:15:17 +0200

    update theme
    + fix blink persistence

---
+++ theme.py
@@ -0,0 +1,15 @@
+class Default():
+    def __init__(self, background, foreground):
+        self.background = background
+        self.foreground = foreground
+
+class Light():
+    def __init__(self, background, foreground):
+        self.background = background
+        self.foreground = foreground  
+        
+class Dark():
+    def __init__(self, background, foreground):
+        self.background = background
+        self.foreground = foreground
+        
